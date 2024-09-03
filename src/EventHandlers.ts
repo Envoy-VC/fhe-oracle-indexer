c52387eeec53e6162032bac96a8676e8f0e31d9a
Vedant Chainani <vedantchainani1084@gmail.com>
2024-09-03 22:37:30 +0530

    chore: add router contract for indexing

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -1,47 +1,20 @@
-/*
- * Please refer to https://docs.envio.dev for a thorough guide on all Envio indexer features
- */
 import {
 	OracleCoordinator,
-	OracleCoordinator_CommitmentDeleted,
-	OracleCoordinator_ConfigSet,
-	OracleCoordinator_ConfigUpdated,
 	OracleCoordinator_OracleAdded,
 	OracleCoordinator_OracleRemoved,
-	OracleCoordinator_OwnershipTransferRequested,
-	OracleCoordinator_OwnershipTransferred,
-	OracleCoordinator_Request,
-	OracleCoordinator_Response,
+	OracleCoordinator_RequestSent,
+	OracleCoordinator_ResponseReceived,
+	OracleRouter,
+	OracleRouter_RequestNotProcessed,
+	OracleRouter_RequestProcessed,
+	OracleRouter_RequestStart,
+	OracleRouter_RequestTimedOut,
+	OracleRouter_SubscriptionCanceled,
+	OracleRouter_SubscriptionConsumerAdded,
+	OracleRouter_SubscriptionConsumerRemoved,
+	OracleRouter_SubscriptionCreated,
+	OracleRouter_SubscriptionOwnerTransferred,
 } from 'generated';
-
-OracleCoordinator.CommitmentDeleted.handler(async ({ event, context }) => {
-	const entity: OracleCoordinator_CommitmentDeleted = {
-		id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-		requestId: event.params.requestId,
-	};
-
-	context.OracleCoordinator_CommitmentDeleted.set(entity);
-});
-
-OracleCoordinator.ConfigSet.handler(async ({ event, context }) => {
-	const entity: OracleCoordinator_ConfigSet = {
-		id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-		maxOracles: event.params.maxOracles,
-		previousConfigBlockNumber: event.params.previousConfigBlockNumber,
-		configDigest: event.params.configDigest,
-	};
-
-	context.OracleCoordinator_ConfigSet.set(entity);
-});
-
-OracleCoordinator.ConfigUpdated.handler(async ({ event, context }) => {
-	const entity: OracleCoordinator_ConfigUpdated = {
-		id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-		config_0: event.params.config,
-	};
-
-	context.OracleCoordinator_ConfigUpdated.set(entity);
-});
 
 OracleCoordinator.OracleAdded.handler(async ({ event, context }) => {
 	const entity: OracleCoordinator_OracleAdded = {
@@ -61,55 +34,136 @@ OracleCoordinator.OracleRemoved.handler(async ({ event, context }) => {
 	context.OracleCoordinator_OracleRemoved.set(entity);
 });
 
-OracleCoordinator.OwnershipTransferRequested.handler(
+OracleCoordinator.RequestSent.handler(async ({ event, context }) => {
+	const entity: OracleCoordinator_RequestSent = {
+		id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+		requestId: event.params.requestId,
+		requestingContract: event.params.requestingContract,
+		requestInitiator: event.params.requestInitiator,
+		subscriptionId: event.params.subscriptionId,
+		subscriptionOwner: event.params.subscriptionOwner,
+		data: event.params.data,
+		callbackGasLimit: event.params.callbackGasLimit,
+		commitment_0: event.params.commitment[0],
+		commitment_1: event.params.commitment[1],
+		commitment_2: event.params.commitment[2],
+		commitment_3: event.params.commitment[3],
+		commitment_4: event.params.commitment[4],
+		commitment_5: event.params.commitment[5],
+	};
+
+	context.OracleCoordinator_RequestSent.set(entity);
+});
+
+OracleCoordinator.ResponseReceived.handler(async ({ event, context }) => {
+	const entity: OracleCoordinator_ResponseReceived = {
+		id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+		requestId: event.params.requestId,
+		transmitter: event.params.transmitter,
+	};
+
+	context.OracleCoordinator_ResponseReceived.set(entity);
+});
+
+OracleRouter.RequestNotProcessed.handler(async ({ event, context }) => {
+	const entity: OracleRouter_RequestNotProcessed = {
+		id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+		requestId: event.params.requestId,
+		coordinator: event.params.coordinator,
+		transmitter: event.params.transmitter,
+		resultCode: event.params.resultCode,
+	};
+
+	context.OracleRouter_RequestNotProcessed.set(entity);
+});
+
+OracleRouter.RequestProcessed.handler(async ({ event, context }) => {
+	const entity: OracleRouter_RequestProcessed = {
+		id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+		requestId: event.params.requestId,
+		subscriptionId: event.params.subscriptionId,
+		transmitter: event.params.transmitter,
+		resultCode: event.params.resultCode,
+		response: event.params.response,
+		err: event.params.err,
+		callbackReturnData: event.params.callbackReturnData,
+	};
+
+	context.OracleRouter_RequestProcessed.set(entity);
+});
+
+OracleRouter.RequestStart.handler(async ({ event, context }) => {
+	const entity: OracleRouter_RequestStart = {
+		id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+		requestId: event.params.requestId,
+		subscriptionId: event.params.subscriptionId,
+		subscriptionOwner: event.params.subscriptionOwner,
+		requestingContract: event.params.requestingContract,
+		requestInitiator: event.params.requestInitiator,
+		data: event.params.data,
+		callbackGasLimit: event.params.callbackGasLimit,
+	};
+
+	context.OracleRouter_RequestStart.set(entity);
+});
+
+OracleRouter.RequestTimedOut.handler(async ({ event, context }) => {
+	const entity: OracleRouter_RequestTimedOut = {
+		id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+		requestId: event.params.requestId,
+	};
+
+	context.OracleRouter_RequestTimedOut.set(entity);
+});
+
+OracleRouter.SubscriptionCanceled.handler(async ({ event, context }) => {
+	const entity: OracleRouter_SubscriptionCanceled = {
+		id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+		subscriptionId: event.params.subscriptionId,
+	};
+
+	context.OracleRouter_SubscriptionCanceled.set(entity);
+});
+
+OracleRouter.SubscriptionConsumerAdded.handler(async ({ event, context }) => {
+	const entity: OracleRouter_SubscriptionConsumerAdded = {
+		id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+		subscriptionId: event.params.subscriptionId,
+		consumer: event.params.consumer,
+	};
+
+	context.OracleRouter_SubscriptionConsumerAdded.set(entity);
+});
+
+OracleRouter.SubscriptionConsumerRemoved.handler(async ({ event, context }) => {
+	const entity: OracleRouter_SubscriptionConsumerRemoved = {
+		id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+		subscriptionId: event.params.subscriptionId,
+		consumer: event.params.consumer,
+	};
+
+	context.OracleRouter_SubscriptionConsumerRemoved.set(entity);
+});
+
+OracleRouter.SubscriptionCreated.handler(async ({ event, context }) => {
+	const entity: OracleRouter_SubscriptionCreated = {
+		id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+		subscriptionId: event.params.subscriptionId,
+		owner: event.params.owner,
+	};
+
+	context.OracleRouter_SubscriptionCreated.set(entity);
+});
+
+OracleRouter.SubscriptionOwnerTransferred.handler(
 	async ({ event, context }) => {
-		const entity: OracleCoordinator_OwnershipTransferRequested = {
+		const entity: OracleRouter_SubscriptionOwnerTransferred = {
 			id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+			subscriptionId: event.params.subscriptionId,
 			from: event.params.from,
 			to: event.params.to,
 		};
 
-		context.OracleCoordinator_OwnershipTransferRequested.set(entity);
+		context.OracleRouter_SubscriptionOwnerTransferred.set(entity);
 	}
 );
-
-OracleCoordinator.OwnershipTransferred.handler(async ({ event, context }) => {
-	const entity: OracleCoordinator_OwnershipTransferred = {
-		id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-		from: event.params.from,
-		to: event.params.to,
-	};
-
-	context.OracleCoordinator_OwnershipTransferred.set(entity);
-});
-
-// OracleCoordinator.Request.handler(async ({ event, context }) => {
-// 	const entity: OracleCoordinator_Request = {
-// 		id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-// 		requestId: event.params.requestId,
-// 		requestingContract: event.params.requestingContract,
-// 		requestInitiator: event.params.requestInitiator,
-// 		subscriptionId: event.params.subscriptionId,
-// 		subscriptionOwner: event.params.subscriptionOwner,
-// 		data: event.params.data,
-// 		callbackGasLimit: event.params.callbackGasLimit,
-// 		commitment_0: event.params.commitment[0],
-// 		commitment_1: event.params.commitment[1],
-// 		commitment_2: event.params.commitment[2],
-// 		commitment_3: event.params.commitment[3],
-// 		commitment_4: event.params.commitment[4],
-// 		commitment_5: event.params.commitment[5],
-// 	};
-
-// 	context.OracleCoordinator_Request.set(entity);
-// });
-
-// OracleCoordinator.Response.handler(async ({ event, context }) => {
-// 	const entity: OracleCoordinator_Response = {
-// 		id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-// 		requestId: event.params.requestId,
-// 		transmitter: event.params.transmitter,
-// 	};
-
-// 	context.OracleCoordinator_Response.set(entity);
-// });
--- test/Test.ts
+++ test/Test.ts
@@ -1,35 +1,39 @@
-import assert from "assert";
-import { 
-  TestHelpers,
-  OracleCoordinator_CommitmentDeleted
-} from "generated";
+import assert from 'assert';
+import { TestHelpers, OracleCoordinator_OracleAdded } from 'generated';
 const { MockDb, OracleCoordinator } = TestHelpers;
 
-describe("OracleCoordinator contract CommitmentDeleted event tests", () => {
-  // Create mock db
-  const mockDb = MockDb.createMockDb();
+describe('OracleCoordinator contract OracleAdded event tests', () => {
+	// Create mock db
+	const mockDb = MockDb.createMockDb();
 
-  // Creating mock for OracleCoordinator contract CommitmentDeleted event
-  const event = OracleCoordinator.CommitmentDeleted.createMockEvent({/* It mocks event fields with default values. You can overwrite them if you need */});
+	// Creating mock for OracleCoordinator contract OracleAdded event
+	const event = OracleCoordinator.OracleAdded.createMockEvent({
+		/* It mocks event fields with default values. You can overwrite them if you need */
+	});
 
-  it("OracleCoordinator_CommitmentDeleted is created correctly", async () => {
-    // Processing the event
-    const mockDbUpdated = await OracleCoordinator.CommitmentDeleted.processEvent({
-      event,
-      mockDb,
-    });
+	it('OracleCoordinator_OracleAdded is created correctly', async () => {
+		// Processing the event
+		const mockDbUpdated = await OracleCoordinator.OracleAdded.processEvent({
+			event,
+			mockDb,
+		});
 
-    // Getting the actual entity from the mock database
-    let actualOracleCoordinatorCommitmentDeleted = mockDbUpdated.entities.OracleCoordinator_CommitmentDeleted.get(
-      `${event.chainId}_${event.block.number}_${event.logIndex}`
-    );
+		// Getting the actual entity from the mock database
+		let actualOracleCoordinatorOracleAdded =
+			mockDbUpdated.entities.OracleCoordinator_OracleAdded.get(
+				`${event.chainId}_${event.block.number}_${event.logIndex}`
+			);
 
-    // Creating the expected entity
-    const expectedOracleCoordinatorCommitmentDeleted: OracleCoordinator_CommitmentDeleted = {
-      id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-      requestId: event.params.requestId,
-    };
-    // Asserting that the entity in the mock database is the same as the expected entity
-    assert.deepEqual(actualOracleCoordinatorCommitmentDeleted, expectedOracleCoordinatorCommitmentDeleted, "Actual OracleCoordinatorCommitmentDeleted should be the same as the expectedOracleCoordinatorCommitmentDeleted");
-  });
+		// Creating the expected entity
+		const expectedOracleCoordinatorOracleAdded: OracleCoordinator_OracleAdded = {
+			id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+			transmitter: event.params.transmitter,
+		};
+		// Asserting that the entity in the mock database is the same as the expected entity
+		assert.deepEqual(
+			actualOracleCoordinatorOracleAdded,
+			expectedOracleCoordinatorOracleAdded,
+			'Actual OracleCoordinatorOracleAdded should be the same as the expectedOracleCoordinatorOracleAdded'
+		);
+	});
 });
